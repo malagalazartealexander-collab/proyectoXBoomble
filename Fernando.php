75fa2cd09a7710699c8b3f1637dda81d21c04126
LeonelVillarroelMendoza <villarroelmendozaleonel@pedropoveda.edu.bo>
2025-11-13 13:52:25 -0400

    Create Fernando.php
    fernando.php

---
+++ Fernando.php
@@ -0,0 +1,4 @@
+<?php
+ echo "Su estructura general se basa en (public class) para nombre de la clase; (public static void main(String[] args){}) para comentar algo del programa y  tipo (nombreMetodo(parametros){}) para definir un método";
+ echo"Cuenta con operadores para numeros, datos primitivos y no primitivos";
+?>
