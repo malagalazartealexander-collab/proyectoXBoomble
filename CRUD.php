db280fad597c83bc0ac2592ba04a2edda3d7ef47
ivanofAS <malagalazartealexander@pedropoveda.edu.bo>
2025-11-25 12:04:16 -0400

    Update CRUD.php

--- CRUD.php
+++ CRUD.php
@@ -3,4 +3,9 @@ echo "<h1>CREATE</h1> ";
 echo "<p>El create es para guardar informacion a partir de un formulario,
 esto con el codigo: <br> INSERT INTO nombre_de_la_tabla (columna1, columna2, columna3) VALUES (valor1, valor2, valor3); <br>
 La informacion que ingrese el usuario ºsera guardada en la base de datos segun los valores que se le asignen a cada columna</p><br>";
+echo "<h1>READ</h1>";
+echo "<p>'READ' es una de las cuatro operaciones fundamentales de CRUD, que significaCrear, leer, actualizar y eliminarLa operación 
+'READ' se refiere a la acción de recuperar y mostrar datos existentes de un sistema de almacenamiento, como una base de datos, sin modificarlos.
+ Esto se suele implementar en aplicaciones como búsquedas, visualización o generación de informes. </p>";
+
 ?>
