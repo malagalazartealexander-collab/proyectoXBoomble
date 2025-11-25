c7d74cad67d762bfb732baa2a1540d6ab4bb997f
PapiDiaz <nigarciadiana@pedropoveda.edu.bo>
2025-11-25 12:26:48 -0400

    IsacD.php

--- isac.php
+++ isac.php
@@ -2,5 +2,5 @@
 echo "JavaScript es un lengiuaje de programacion el cual da covertiura a una mayor gama de codigos y estilos que pueden ser utilizados"
 echo "Para la sinbtaxis de JavaScriprt se utiliza distintos terminos en Visual Studio Code se necesita llamar con <script> para dar inicio a este formato de codgo emn el cual se llega a utilizar distintas llaes{}, corchetes[], y clases para su correcto funcionamient",
 echo "Para las estructuras repetitivas se utiliza mas que nada For y While. For en caso de que la condicion sea un nuimero de interaacciones y while si se necesita una condcione externa a numeros dentro del codigo"
-echo "POO es al estrucrura de organizacion dada a objetos en donde se llega a actualizar uy mostrar nuevas formas de como dar a cononcer a estos"
+echo "POO es al estrucrura de organizacion dada a objetos en donde se llega a actualizar uy mostrar nuevas formas de como dar a cononcer a estos nuevos metodos" 
 ?>
