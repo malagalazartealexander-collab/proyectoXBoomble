08b1fea8f3793c0d9d7928ab4db946f0f851e3ab
LeonelVillarroelMendoza <villarroelmendozaleonel@pedropoveda.edu.bo>
2025-11-25 11:45:40 -0400

    Crear LeonelV.phpf

--- LeonelV.php
+++ LeonelV.php
@@ -1,4 +1,10 @@
 <?php
-echo "Java es un lenguaje de programación orientado a objetos y de propósito general";
-echo "La sintaxis de java es";
+echo "Java es un lenguaje de programación orientado a objetos y de propósito general <br>";
+echo "La sintaxis de java es: <br> Clase: public class HolaMundo { <br>
+    public static void main(String[] args) { <br>
+        System.out.println(\"Hola Mundo\"); }  }";
+echo "Las estructuras repetitivas de java es for, while, do while <br>";
+echo "En POO en Java, los atributos son variables que definen las características (como color o modelo) de un objeto, mientras que los métodos son funciones que definen las acciones que puede realizar (como arrancar() o acelerar()). En una línea, los atributos son los datos y los métodos son los comportamientos de una clase o objeto<br>";
+echo "La herencia en Java es un concepto de la programación orientada a objetos que permite que una clase (la hija o subclase) herede propiedades y métodos de otra clase existente (la padre o superclase)";
 ?>
+
