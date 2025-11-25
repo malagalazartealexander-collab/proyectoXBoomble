93c11af37a7792c5e837a34412497d30a0c026a7
LeonelVillarroelMendoza <villarroelmendozaleonel@pedropoveda.edu.bo>
2025-11-25 11:59:03 -0400

    Crear CRUD.php

---
+++ CRUD.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>CREATE</h1> ";
+echo "<p>El create es para guardar informacion a partir de un formulario,
+esto con el codigo: <br> INSERT INTO nombre_de_la_tabla (columna1, columna2, columna3) VALUES (valor1, valor2, valor3); <br>
+La informacion que ingrese el usuario ºsera guardada en la base de datos segun los valores que se le asignen a cada columna</p><br>";
+?>
